36e7a646522a08974746b0143bd97a32843909f5
marchtaylor <marchtaylor@gmail.com>
2015-01-31 14:38:50 +0100

    ypr function update 2

--- R/ypr.R
+++ R/ypr.R
@@ -5,10 +5,14 @@
 #' @param params List of parameters for the population. Applies a single cohort
 #' simulation for the initial population state using \code{\link[fishdynr]{cohortSim}}
 #' function
-#' @param adj.params List of parameters combinations to use for yield per recruit
+#' @param adj.params List of 2 parameters combinations to use for yield per recruit
 #' analysis. In the case of trawl-type selectivity, these should be levels for
 #' F and knife-edge length at capture. For gillnet-type selectivity, these should be
 #' levels for F and mesh_size.
+#' 
+#' @return A list of parameter combinations used in the yield-per-recruit
+#' exploration, plus matrix output for yield (\code{Y}) and spawning stock
+#' biomass (\code{SB}) given the parameter combinations (\code{adj.params.comb}).
 #'    
 #' @examples
 #' # Trawl-type
@@ -43,13 +47,13 @@
 #' ))
 #' op <- par(mfcol=c(1,2))
 #' # Yield
-#' image(x=res$F, y=res$knife_edge, z=res$Y, col=pal(100))
-#' contour(x=res$F, y=res$knife_edge, z=res$Y, add=TRUE)
+#' image(x=res$F, y=res$mesh_size, z=res$Y, col=pal(100))
+#' contour(x=res$F, y=res$mesh_size, z=res$Y, add=TRUE)
 #' mtext("Yield", line=0.5, side=3)
 #' # Relative spawning biomass
 #' SB_F0 <- res$SB[which(res$F==0),1]
-#' image(x=res$F, y=res$knife_edge, z=res$SB/SB_F0, col=pal(100))
-#' contour(x=res$F, y=res$knife_edge, z=res$SB/SB_F0, add=TRUE)
+#' image(x=res$F, y=res$mesh_size, z=res$SB/SB_F0, col=pal(100))
+#' contour(x=res$F, y=res$mesh_size, z=res$SB/SB_F0, add=TRUE)
 #' mtext("Rel. Spawning Biomass", line=0.5, side=3)
 #' par(op)
 #' 
@@ -62,10 +66,10 @@ ypr <- function(params, adj.params){
   res[[2]] <- sort(res[[2]])
   res$Y <- matrix(NaN, length(res[[1]]), length(res[[2]]))
   res$SB <- matrix(NaN, length(res[[1]]), length(res[[2]]))  
-  var.params <- expand.grid(res[[1]], res[[2]])
-  names(var.params) <- names(adj.params)
+  res$adj.params.comb <- expand.grid(res[[1]], res[[2]])
+  names(res$adj.params.comb) <- names(adj.params)
   for(i in seq(res$Y)){
-    params[names(var.params)] <- var.params[i,]
+    params[names(res$adj.params.comb)] <- res$adj.params.comb[i,]
     cS.res <- cohortSim(params, t_incr=0.1)
     res$Y[i] <- cS.res$Y
     res$SB[i] <- cS.res$SB   
